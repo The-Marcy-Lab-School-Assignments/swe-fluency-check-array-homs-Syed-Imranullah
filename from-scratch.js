d594655e9c1b9a38308b1502332ffde5bf7e97b4
Syed <xayanmay@gmail.com>
2025-11-03 10:08:27 -0500

    Done

--- from-scratch.js
+++ from-scratch.js
@@ -6,30 +6,28 @@
 /* --------------STRING PROBLEMS--------------- */
 /* -------------------------------------------- */
 
-const sampleWords = ['dog', 'blueberry', 'alabama', 'earth', 'football'];
+const sampleWords = ["dog", "blueberry", "alabama", "earth", "football"];
 
 // Problem 1
 const printEveryWord = (words) => {
   return words.forEach((word) => {
-    console.log(word)
-  }
-  )
+    console.log(word);
+  });
 };
 
 //printEveryWord(sampleWords);
 
 // Problem 2
 const getStringLengths = (strings) => {
-  return strings.map((word) => word.length)
+  return strings.map((word) => word.length);
 };
 
 //console.log(getStringLengths(sampleWords))
 
 // Problem 3
 const sortWordsZtoA = (words) => {
-  words.sort()
-  words.reverse()
-
+  words.sort();
+  words.reverse();
 };
 //sortWordsZtoA(sampleWords);
 //console.log(sampleWords);
@@ -42,52 +40,65 @@ const sampleNumbers = [7, 3, 9, 1, 4, 8, 2, 10, 5, 6];
 
 // Problem 4
 const getEvenNumbers = (nums) => {
-  return nums.filter((num) => num % 2 === 0)
+  return nums.filter((num) => num % 2 === 0);
 };
 //console.log(getEvenNumbers(sampleNumbers));
 
-
 // Problem 5
 const getLargestNumber = (numbers) => {
-
+  return numbers.reduce((currentLargest, currentnum) => {
+    if (currentnum > currentLargest) {
+      return currentnum;
+    } else {
+      return currentLargest;
+    }
+  });
 };
+//console.log(getLargestNumber(sampleNumbers))
 
 /* -------------------------------------------------- */
 /* --------------OBJECT ARRAY PROBLEMS--------------- */
 /* -------------------------------------------------- */
 
 const sampleUsers = [
-  { name: 'Charlie', isActive: true, age: 18, score: 80 },
-  { name: 'Edward', isActive: true, age: 30, score: 60 },
-  { name: 'Bob', isActive: true, age: 25, score: 90 },
-  { name: 'Diana', isActive: false, age: 35, score: 70 },
-  { name: 'Alice', isActive: false, age: 28, score: 100 },
+  { name: "Charlie", isActive: true, age: 18, score: 80 },
+  { name: "Edward", isActive: true, age: 30, score: 60 },
+  { name: "Bob", isActive: true, age: 25, score: 90 },
+  { name: "Diana", isActive: false, age: 35, score: 70 },
+  { name: "Alice", isActive: false, age: 28, score: 100 },
 ];
 
 // Problem 6
 const printUserNamesAndScores = (users) => {
-
+  users.forEach((user) => {
+    console.log(`${user.name} scored ${user.score} points.`);
+  });
 };
 
+//printUserNamesAndScores(sampleUsers);
 
 // Problem 7
 const getUserNames = (users) => {
-
+  return users.map((user) => user.name);
 };
-
+//console.log(getUserNames(sampleUsers))
 
 // Problem 8
 const getActiveUsersUnder30 = (users) => {
-
+  return users.filter((user) => user.isActive && user.age < 30);
 };
-
+//console.log(getActiveUsersUnder30(sampleUsers));
 
 // Problem 9
 const getTotalScore = (users) => {
-
+  return users.reduce((total, user) => total + user.score, 0);
 };
+// console.log(getTotalScore(sampleUsers))
 
 // Problem 10
 const sortUsersByScoreDescending = (users) => {
-
+  return users.sort((a, b) => b.score - a.score);
 };
+
+sortUsersByScoreDescending(sampleUsers);
+console.log(sampleUsers);
